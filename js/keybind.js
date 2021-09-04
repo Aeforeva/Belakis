21fc645896cb0eaa7494ea6ca942e02242a00094
Aeforeva <83841383+Aeforeva@users.noreply.github.com>
2021-09-05 01:30:07 +0300

    Add files via upload
    add 3 random type

--- js/keybind.js
+++ js/keybind.js
@@ -1,5 +1,6 @@
 document.addEventListener("keydown", function (params) {
   if (params.code === "Space") {
+    params.preventDefault();
     start();
   } else if (params.code !== "F5") {
     check(params.code[3]);
--- js/keys.js
+++ js/keys.js
@@ -1,10 +1,57 @@
 const keys = ["Q", "W", "E", "R", "A", "S", "D", "F"];
 let randomKeys = [];
+let createRandomKeys = createRandomKeys3;
 
-function createRandomKeys() {
+//Set random type from dropdown
+function setRandomType(params) {
+  switch (params) {
+    case "1":
+      createRandomKeys = createRandomKeys1;
+      break;
+    case "2":
+      createRandomKeys = createRandomKeys2;
+      break;
+    case "3":
+      createRandomKeys = createRandomKeys3;
+      break;
+  }
+}
+
+//Type-1 Full random
+function createRandomKeys1() {
+  console.log("createRandomKeys Type 1 called");
   for (let index = 0; index < keys.length; index++) {
     const element = keys[Math.floor(Math.random() * keys.length)];
     randomKeys.push(element);
+  }
+}
+
+//Type-2 No more then 2 same
+function createRandomKeys2() {
+  console.log("createRandomKeys Type 2 called");
+  const keys2 = keys.concat(keys);
+  for (let index = 0; index < keys.length; index++) {
+    const roll = Math.floor(Math.random() * keys2.length);
+    randomKeys.push(keys2[roll]);
+    keys2.splice(roll, 1);
+  }
+}
+
+//Type-3 No 3 same in a row
+function createRandomKeys3() {
+  console.log("createRandomKeys Type 3 called");
+  const keys3 = keys;
+  for (let i = 0; i < keys.length; i++) {
+    const index = Math.floor(Math.random() * keys3.length);
+    const element = keys3[index];
+    if (element == randomKeys[i - 1] && element == randomKeys[i - 2]) {
+      keys3.splice(index, 1);
+      const reroll = Math.floor(Math.random() * keys3.length);
+      randomKeys.push(keys3[reroll]);
+      keys3.push(element);
+    } else {
+      randomKeys.push(element);
+    }
   }
 }
 
@@ -15,7 +62,3 @@ function renderRandomKeys() {
   }
   wasd.innerHTML = html;
 }
-
-//TODO 
-//make impossible to create 3 same key in row ? 
-//or make no more then 2 same key in [] ?
